0c9e03fafca27ba1b27839667a1f250d8b5b450d
Shaps Benkau <shapsuk@me.com>
2026-06-21 17:05:09 +0100

    Build script now accept release config

--- build.sh
+++ build.sh
@@ -3,12 +3,40 @@ set -euo pipefail
 
 PORT=9999
 URL="http://127.0.0.1:${PORT}/App/index.html"
+CONFIGURATION="debug"
+ARGS=()
 
-swiftly run swift package --swift-sdk swift-6.3.2-RELEASE_wasm js --product MOOD --use-cdn
+while (($#)); do
+  case "$1" in
+    -c|--configuration)
+      CONFIGURATION="${2:-debug}"
+      shift 2
+      ;;
+    -c=*)
+      CONFIGURATION="${1#-c=}"
+      shift
+      ;;
+    --configuration=*)
+      CONFIGURATION="${1#--configuration=}"
+      shift
+      ;;
+    *)
+      ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if ((${#ARGS[@]})); then
+  swiftly run swift package --swift-sdk swift-6.3.2-RELEASE_wasm js --product MOOD --use-cdn -c "${CONFIGURATION}" "${ARGS[@]}"
+else
+  swiftly run swift package --swift-sdk swift-6.3.2-RELEASE_wasm js --product MOOD --use-cdn -c "${CONFIGURATION}"
+fi
 
 echo
 echo "MOOD running at:"
 echo "${URL}"
+echo "Configuration: ${CONFIGURATION}"
 echo
 echo "Press Ctrl-C to stop."
 
