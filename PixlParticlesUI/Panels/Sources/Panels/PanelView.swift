5af8382cb778cd954accb3be08c64f3c18071b51
Shaps Benkau <shapsuk@me.com>
2026-08-22 21:13:50 +0100

    Dragging is now working correctly

--- PixlParticlesUI/Panels/Sources/Panels/PanelView.swift
+++ PixlParticlesUI/Panels/Sources/Panels/PanelView.swift
@@ -18,7 +18,7 @@ nonisolated public struct PanelView<ID, Content>: View, ~Sendable where ID: Hash
             Group(subviews: content) { subviews in
                 let orderedSubviews = subviews.ordered(by: customization)
 
-                ZStack {
+                ZStack(alignment: .topLeading) {
                     ForEach(orderedSubviews) { subview in
                         ContentView(
                             subview: subview,
@@ -50,7 +50,7 @@ private enum PanelKind: String, Identifiable, Sendable {
 
         PanelView(customization: $customization) {
             Panel(id: .properties) {
-                FittingScrollView {
+                ScrollView {
                     Text("Hello")
                         .padding()
                 }
@@ -67,29 +67,4 @@ private enum PanelKind: String, Identifiable, Sendable {
         .scenePadding()
     }
     .frame(width: 800, height: 600)
-}
-
-struct FittingScrollView<Content: View>: View {
-    @State private var contentHeight = 0.0
-    @ViewBuilder private let content: Content
-
-    init(@ViewBuilder content: () -> Content) {
-        self.content = content()
-    }
-
-    var body: some View {
-        ScrollView {
-            content
-                .onGeometryChange(for: Double.self) { proxy in
-                    proxy.size.height
-                } action: { height in
-                    contentHeight = height
-                }
-        }
-        .frame(
-            idealHeight: contentHeight,
-            maxHeight: contentHeight
-        )
-        .scrollBounceBehavior(.basedOnSize)
-    }
 }
